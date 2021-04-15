844e4b39eee3600b47d482ab043e151b4c9cfe83
Martin Johansson <maxamartin1999@gmail.com>
2021-04-15 15:56:38 +0200

    Added basic in score

--- TetrisClassLibrary/Score.cs
+++ TetrisClassLibrary/Score.cs
@@ -8,5 +8,20 @@ namespace TetrisClassLibrary
 {
     class Score
     {
+        private int Points { get; set; }
+        private int Level { get; set; }
+
+        private int GetGravity(int points)
+        {
+            switch (points)
+            {
+                case < 200:
+                    return 19;
+                case < 300:
+                    return 18;
+                default:
+                    return 20;
+            }
+        }
     }
 }
